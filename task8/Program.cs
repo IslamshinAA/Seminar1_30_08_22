5037a489c64f2ca44cca73d7b82d029886216985
A.Islamshin <arshavin0511@gmail.com>
2022-08-30 21:10:43 +0300

    task_8_edit

---
+++ task8/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Задача 8: Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8 */
+
+Console.WriteLine("Введите положительное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int i = num;
+if(i % 2 == 0)
+    while (i > 0)
+{
+    Console.Write(i + ", ");
+    i = i -2;
+}
+else if(i % 2 !=0)
+    i--;
+    while (i > 0)
+{
+    Console.Write(i + ", ");
+    i = i -2;
+}
+
+    
+    
+
+
+
